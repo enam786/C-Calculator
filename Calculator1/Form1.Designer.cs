7883893bab74384ce2261bb2ec2677e98b5ab7a6
ENAM <russell.m2006@gmail.com>
2018-12-09 02:46:10 -0500

    C# calculator

---
+++ Calculator1/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Calculator1
+{
+    partial class frmCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnModulus = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnExponent = new System.Windows.Forms.Button();
+            this.btnDivided = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.picCalculator = new System.Windows.Forms.PictureBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.lblCalculator = new System.Windows.Forms.Label();
+            this.lblSolution = new System.Windows.Forms.Label();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.lblEqual = new System.Windows.Forms.Label();
+            this.lblFirstNumber = new System.Windows.Forms.Label();
+            this.lblSecondNumber = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picCalculator)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnModulus
+            // 
+            this.btnModulus.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModulus.Location = new System.Drawing.Point(192, 217);
+            this.btnModulus.Name = "btnModulus";
+            this.btnModulus.Size = new System.Drawing.Size(78, 59);
+            this.btnModulus.TabIndex = 4;
+            this.btnModulus.TabStop = false;
+            this.btnModulus.Text = "%";
+            this.btnModulus.UseVisualStyleBackColor = true;
+            this.btnModulus.Click += new System.EventHandler(this.btnpercentage_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(108, 217);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(78, 59);
+            this.btnMultiply.TabIndex = 3;
+            this.btnMultiply.TabStop = false;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(24, 217);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(78, 59);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.TabStop = false;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnExponent
+            // 
+            this.btnExponent.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExponent.Location = new System.Drawing.Point(192, 279);
+            this.btnExponent.Name = "btnExponent";
+            this.btnExponent.Size = new System.Drawing.Size(78, 54);
+            this.btnExponent.TabIndex = 7;
+            this.btnExponent.TabStop = false;
+            this.btnExponent.Text = "^";
+            this.btnExponent.UseVisualStyleBackColor = true;
+            this.btnExponent.Click += new System.EventHandler(this.btnExponent_Click);
+            // 
+            // btnDivided
+            // 
+            this.btnDivided.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivided.Location = new System.Drawing.Point(108, 279);
+            this.btnDivided.Name = "btnDivided";
+            this.btnDivided.Size = new System.Drawing.Size(78, 54);
+            this.btnDivided.TabIndex = 6;
+            this.btnDivided.TabStop = false;
+            this.btnDivided.Text = "/";
+            this.btnDivided.UseVisualStyleBackColor = true;
+            this.btnDivided.Click += new System.EventHandler(this.btnDivided_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtract.Location = new System.Drawing.Point(24, 277);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(78, 56);
+            this.btnSubtract.TabIndex = 5;
+            this.btnSubtract.TabStop = false;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(362, 217);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(97, 59);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.TabStop = false;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(362, 277);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(97, 56);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.TabStop = false;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // picCalculator
+            // 
+            this.picCalculator.Image = global::Calculator1.Properties.Resources.cal;
+            this.picCalculator.Location = new System.Drawing.Point(362, 12);
+            this.picCalculator.Name = "picCalculator";
+            this.picCalculator.Size = new System.Drawing.Size(116, 79);
+            this.picCalculator.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCalculator.TabIndex = 8;
+            this.picCalculator.TabStop = false;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(161, 114);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(94, 20);
+            this.txtNumber2.TabIndex = 1;
+            this.txtNumber2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(12, 111);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber1.TabIndex = 0;
+            this.txtNumber1.Text = "8";
+            this.txtNumber1.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // lblCalculator
+            // 
+            this.lblCalculator.BackColor = System.Drawing.Color.Lime;
+            this.lblCalculator.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalculator.ForeColor = System.Drawing.Color.Black;
+            this.lblCalculator.Location = new System.Drawing.Point(12, 12);
+            this.lblCalculator.Name = "lblCalculator";
+            this.lblCalculator.Size = new System.Drawing.Size(321, 68);
+            this.lblCalculator.TabIndex = 11;
+            this.lblCalculator.Text = "Super Calculator";
+            this.lblCalculator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCalculator.Click += new System.EventHandler(this.lblCalculator_Click);
+            // 
+            // lblSolution
+            // 
+            this.lblSolution.BackColor = System.Drawing.Color.Lime;
+            this.lblSolution.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSolution.Location = new System.Drawing.Point(314, 113);
+            this.lblSolution.Name = "lblSolution";
+            this.lblSolution.Size = new System.Drawing.Size(209, 23);
+            this.lblSolution.TabIndex = 12;
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lblOperator.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperator.Location = new System.Drawing.Point(118, 111);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(37, 23);
+            this.lblOperator.TabIndex = 13;
+            // 
+            // lblEqual
+            // 
+            this.lblEqual.BackColor = System.Drawing.Color.Lime;
+            this.lblEqual.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEqual.Location = new System.Drawing.Point(267, 112);
+            this.lblEqual.Name = "lblEqual";
+            this.lblEqual.Size = new System.Drawing.Size(41, 24);
+            this.lblEqual.TabIndex = 14;
+            this.lblEqual.Text = " =";
+            // 
+            // lblFirstNumber
+            // 
+            this.lblFirstNumber.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstNumber.Location = new System.Drawing.Point(13, 135);
+            this.lblFirstNumber.Name = "lblFirstNumber";
+            this.lblFirstNumber.Size = new System.Drawing.Size(99, 26);
+            this.lblFirstNumber.TabIndex = 3;
+            this.lblFirstNumber.Text = "First Number";
+            this.lblFirstNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblFirstNumber.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblSecondNumber
+            // 
+            this.lblSecondNumber.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecondNumber.Location = new System.Drawing.Point(161, 141);
+            this.lblSecondNumber.Name = "lblSecondNumber";
+            this.lblSecondNumber.Size = new System.Drawing.Size(94, 26);
+            this.lblSecondNumber.TabIndex = 16;
+            this.lblSecondNumber.Text = "Second Number";
+            // 
+            // frmCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 371);
+            this.Controls.Add(this.lblSecondNumber);
+            this.Controls.Add(this.lblFirstNumber);
+            this.Controls.Add(this.lblEqual);
+            this.Controls.Add(this.lblOperator);
+            this.Controls.Add(this.lblSolution);
+            this.Controls.Add(this.lblCalculator);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.picCalculator);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnDivided);
+            this.Controls.Add(this.btnExponent);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnModulus);
+            this.Name = "frmCalculator";
+            this.Text = "Super Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.picCalculator)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnModulus;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnExponent;
+        private System.Windows.Forms.Button btnDivided;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox picCalculator;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.Label lblCalculator;
+        private System.Windows.Forms.Label lblSolution;
+        private System.Windows.Forms.Label lblOperator;
+        private System.Windows.Forms.Label lblEqual;
+        private System.Windows.Forms.Label lblFirstNumber;
+        private System.Windows.Forms.Label lblSecondNumber;
+    }
+}
+
---
+++ Calculator1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator1
+{
+    public partial class frmCalculator : Form
+    {
+        public frmCalculator()
+        {
+            InitializeComponent();
+        }
+        double number1 = 0;
+        double number2 = 0;
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCalculator_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+     if (double.TryParse(txtNumber1.Text, out number1) && double.TryParse(txtNumber2.Text, out number2))
+            {
+                lblSolution.Text = (number1 + number2).ToString();
+                lblOperator.Text = "+";
+            }
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            if (double. TryParse(txtNumber1.Text, out number1) && double.TryParse(txtNumber2.Text, out number2))
+                
+            {
+                lblSolution.Text = (number1 - number2).ToString();
+                lblOperator.Text = "-";
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNumber1.Clear();
+            txtNumber2.Clear();
+            lblSolution.Text = "";
+           
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtNumber1.Text, out number1) && double.TryParse(txtNumber2.Text, out number2))
+
+            {
+                lblSolution.Text = (number1 * number2).ToString();
+                lblOperator.Text = "*";
+            }
+                
+        }
+
+        private void btnpercentage_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtNumber1.Text, out number1) && double.TryParse(txtNumber2.Text, out number2))
+
+            {
+                lblSolution.Text = (number1 % number2).ToString();
+                lblOperator.Text = "%";
+            }
+        }
+
+        private void btnDivided_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtNumber1.Text, out number1) && double.TryParse(txtNumber2.Text, out number2))
+
+            {
+                lblSolution.Text = (number1 / number2).ToString();
+                lblOperator.Text = "/";
+            }
+        }
+
+        private void btnExponent_Click(object sender, EventArgs e)
+        {
+
+            if (double.TryParse(txtNumber1.Text, out number1) && double.TryParse(txtNumber2.Text, out number2))
+
+            {
+                lblSolution.Text = Math.Pow(number1, number2).ToString();
+                lblOperator.Text = "^";
+            }
+        }
+    }
+}
